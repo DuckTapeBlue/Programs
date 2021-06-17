1397b578f67904a647eb07125be43353ef7db427
DuckTapeBlue <85902085+DuckTapeBlue@users.noreply.github.com>
2021-06-17 11:52:11 -0700

    Add files via upload

---
+++ dict_practice.py
@@ -0,0 +1,25 @@
+grocery_list = {"chicken":"$1.59", "beef":"$1.99", "cheese":"$1.00", "milk":"$2.50"}
+hair_color = {"evan":"brown", "bernadette":"black", "tom":"blond", "joe":"blue"}
+
+
+chicken_price = grocery_list["chicken"]
+beef_price = grocery_list["beef"]
+cheese_price = grocery_list["cheese"]
+milk_price = grocery_list["milk"]
+
+print("chicken" + " " + chicken_price)
+print("beef" + " " + beef_price)
+print("cheese" + " " + cheese_price)
+print("milk" + " " + milk_price)
+
+evan_hair_color = hair_color["evan"]
+bernadette_hair_color = hair_color["berndatte"]
+tom_hair_color = hair_color["tom"]
+joe_hair_color = hair_color["joe"]
+
+
+print("evan" + " " + evan_hair_color)
+print("bernadette" + " " + bernadette_hair_color)
+print("tom" + " " + tom_hair_color)
+print("joe" + " " + joe_hair_color)
+
