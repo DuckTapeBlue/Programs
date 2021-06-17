f31befc4b9f7b16830ebb4aa92b9da70c913c508
DuckTapeBlue <85902085+DuckTapeBlue@users.noreply.github.com>
2021-06-17 11:51:11 -0700

    Add files via upload

---
+++ cityList.py
@@ -0,0 +1,18 @@
+empty_list = []
+city_list = ["Oakland, Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(city_list)
+print(city_list[5])
+
+
+
+
+
+
+
+ice_cream = ["vanilla", "chocolate", "mint", "caramel", "coffee", "almond", "banana", "cherry", "lemon", "peach"]
+print(ice_cream)
+
+
+three_cities = city_list[0:3]
+print( three_cities)
+three_cities[0] = "Memphis"
