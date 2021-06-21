9751afd0908eb92bbf7757ef90f11eb640172344
DuckTapeBlue <evan.kirchstetter@gmail.com>
2021-06-21 09:42:04 -0700

    lab 4

--- Program4.py
+++ Program4.py
@@ -22,3 +22,12 @@ print(ListA)
 
 ListA.append(5)
 print(ListA)
+
+
+Menu = {"Burgers": 12.99, "Fries":3.99, "Shakes":1.50}
+
+def foodSum(item1,item2):
+    sum = item1 + item2
+    print("The total price of your order of " + str(item1) + " and " + str(item2) + " is " + str(sum))
+
+foodSum(Menu["Burgers"], Menu["Fries"])
